a3bfb48a1258b3ca014f274be79657d2de88502d
Alexis Chaignon <alexis.chaignon@gmail.com>
2025-05-09 14:49:34 +0200

    feat: Add factories for Anime, Movie, Studio, Director, Character, and Genre

---
+++ src/Factory/DirectorFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Director;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Director>
+ */
+final class DirectorFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Director::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'name' => self::faker()->name(),
+            'birthYear' => self::faker()->year(),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Director $director): void {})
+        ;
+    }
+}
---
+++ src/Factory/MovieFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Movie;
+use App\Factory\StudioFactory;
+use App\Factory\DirectorFactory;
+use App\Factory\CharacterFactory;
+use App\Factory\GenreFactory;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Movie>
+ */
+final class MovieFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Movie::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'title' => self::faker()->sentence(3),
+            'releaseYear' => self::faker()->year(),
+            'synopsis' => self::faker()->paragraph(),
+            'duration' => self::faker()->numberBetween(60, 130),
+            'studio' => StudioFactory::new(),
+            'directors' => DirectorFactory::new()->many(1, 2),
+            'characters' => CharacterFactory::new()->many(2, 5),
+            'genres' => GenreFactory::new()->many(1, 3),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Movie $movie): void {})
+        ;
+    }
+}
