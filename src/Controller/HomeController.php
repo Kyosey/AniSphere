cf7ebedeba5362b88ce76ab6d226c95ac1583404
Alexis Chaignon <alexis.chaignon@gmail.com>
2025-05-10 21:34:42 +0200

    feat: Add Movie list in homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AnimeRepository;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(AnimeRepository $animeRepository): Response
+    public function index(AnimeRepository $animeRepository, MovieRepository $movieRepository): Response
     {
         // Récupérer les derniers animes (limité à 6 par exemple)
         $latestAnimes = $animeRepository->findBy([], ['releaseYear' => 'DESC'], 6);
+        
+        // Récupérer les derniers films (limité à 6 par exemple)
+        $latestMovies = $movieRepository->findBy([], ['releaseYear' => 'DESC'], 6);
 
         return $this->render('home/index.html.twig', [
-            'animes' => $latestAnimes
+            'animes' => $latestAnimes,
+            'movies' => $latestMovies
         ]);
     }
 }
